373fcb95bc297a962ae2b73445b707061ea1f40c
NirojEdulab <niroj@edulab.in>
2024-08-30 11:35:05 +0530

    added public path

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,7 +1,11 @@
 import multer from "multer";
+import fs from 'fs';
+import path from 'path';
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
+    const dir = path.join('public', 'temp');
+    fs.mkdirSync(dir, { recursive: true });
     cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
